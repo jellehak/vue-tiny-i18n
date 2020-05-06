7f3db1640a735ac52dad96965927e8f9079eff5d
Jelle Hak <jellehak@gmail.com>
2020-05-06 13:06:06 +0200

    Better count handling

--- index.js
+++ index.js
@@ -7,14 +7,43 @@ const defaultOptions = {
   messages: {},
   fallbackTranslation: '__NO_TRANSLATION__',
 
-  // Add loggers, push keys to API, ...
+  // ==========
+  // Hooks to do your own loggers, push keys to API, ...
+  /**
+   * Called to get key
+   *
+   * @param {*} key
+   * @param {*} { messages = {}, locale, ns = 'common' }
+   * @returns
+   */
+  getKey (key, { messages = {}, locale, ns = 'common' }) {
+    const translation = messages[locale][key]
+    return translation
+  },
+
+  /**
+   * Called if getKey returns false
+   *
+   * @param {*} key
+   * @param {*} { messages, locale, fallbackLocale }
+   * @returns
+   */
   onMissingKey (key, { messages, locale, fallbackLocale }) {
     console.log(`Missing key for language "${locale}": ${key}`)
     return messages[fallbackLocale][key]
   },
-  getKey (key, { messages = {}, locale, ns = 'common' }) {
-    const translation = messages[locale][key]
-    return translation
+
+  /**
+   * Called after getKey or onMissingKey
+   *
+   * @param {*} key
+   * @param {*} { messages = {}, locale, ns = 'common' }
+   */
+  parseTranslation (translation, ctx = {}) {
+    // Parse variables like {count}, ...
+    return translation.replace(/\{(.*?)\}/g, function (match, token) {
+      return ctx[token]
+    })
   }
 }
 
@@ -36,51 +65,74 @@ export default {
       ..._options
     }
 
-    // Bind
-    Vue.prototype[vueProp] = Vue.observable(options)
-
     // Get hookable methods
     const { getKey } = options
 
+    const $t = function (key, settings = {
+      context: '',
+      count: 1
+    }) {
+      const combinedOptions = {
+        ...options,
+        ...settings
+      }
+
+      const translation = getKey(key, combinedOptions) ||
+        combinedOptions.onMissingKey(key, combinedOptions) ||
+        combinedOptions.fallbackTranslation
+
+      return combinedOptions.parseTranslation(translation, combinedOptions)
+    }
+
+    // Special for some compatibility with https://kazupon.github.io/vue-i18n/guide/pluralization.html#accessing-the-number-via-the-pre-defined-argument
+    const $tc = function (key, count = 1, settings = {}) {
+      const combinedOptions = {
+        ...options,
+        ...settings,
+        count
+      }
+
+      const translation = getKey(key, combinedOptions) ||
+        combinedOptions.onMissingKey(key, combinedOptions) ||
+        combinedOptions.fallbackTranslation
+
+      // Replace count tag e.g. {count}
+      const { countTag = '{count}', countSeperator = '|' } = combinedOptions
+      const parts = translation.split(countSeperator)
+
+      const clamp = (value, min, max) => Math.min(Math.max(value, min), max)
+      // Cases:
+      // 1: odd: '{count} car',
+      // 2: car: 'car | cars',
+      // 3: apple: 'no apples | one apple | {count} apples'
+
+      // parts index calculator table
+      //
+      // count   length => 1   2   3
+      //
+      // -2                0   0   0
+      // -1                0   0   0
+      // 0                 0   0   0
+      // 1                 0   0   1
+      // 2                 0   1   2
+      // 3                 0   1   2
+      // 4                 0   1   2
+
+      // included edge case clamp count == 1 and partslength == 2
+      const value = count === 1 && parts.length === 2 ? 0 : count
+      const partsIndex = clamp(value, 0, parts.length - 1)
+
+      const part = parts[partsIndex] || options.fallbackTranslation
+      return part.replace(countTag, count)
+    }
+
+    // Bind to Vue
+    Vue.prototype[vueProp] = Vue.observable(options)
+
     Vue.mixin({
-      computed: {
-        $t () {
-          return function (key, settings = {
-            context: '',
-            count: 1
-          }) {
-            const combinedOptions = {
-              ...options,
-              ...settings
-            }
-            return getKey(key, combinedOptions) ||
-              options.onMissingKey(key, combinedOptions) ||
-              options.fallbackTranslation
-          }
-        },
-
-        $tc () {
-          return function (key, settings = { count: 1 }) {
-            const combinedOptions = {
-              ...options,
-              ...settings
-            }
-
-            const translation = getKey(key, combinedOptions) ||
-              options.onMissingKey(key, combinedOptions) ||
-              options.fallbackTranslation
-
-            // Settings
-            const count = settings.count ? settings.count : settings
-            const { countTag, countSeperator } = combinedOptions
-
-            const parts = translation.split(countSeperator)
-
-            // Replace count tag
-            const part = parts[count > 1 ? 1 : 0] || options.fallbackTranslation
-            return part.replace(countTag, count)
-          }
-        }
+      methods: {
+        $t,
+        $tc
       }
 
     })
